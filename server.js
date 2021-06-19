042a9bc7ce56d848e24788b8a1e354b9662eeade
Kota Kanazawa <kota0818@gmail.com>
2021-06-19 15:15:00 +0900

    オブジェクトタイプ

--- server.js
+++ server.js
@@ -3,18 +3,38 @@ const { graphqlHTTP } = require('express-graphql')
 const { buildSchema } = require('graphql')
 
 const schema = buildSchema(`
+  type RandomDie {
+    numSides: Int!
+    rollOnce: Int!
+    roll(numRolls: Int!): [Int]
+  }
+
   type Query {
-    rollDice(numDice: Int!, numSides: Int): [Int]
+    getDie(numSides: Int): RandomDie
   }
 `)
 
-const root = {
-  rollDice: ({numDice, numSides}) => {
+class RandomDie {
+  constructor(numSides) {
+    this.numSides = numSides
+  }
+
+  rollOnce() {
+    return 1 + Math.floor(Math.random() * this.numSides)
+  }
+
+  roll({numRolls}) {
     let output = []
-    for(var i = 0; i < numDice; i++) {
-      output.push(1 + Math.floor(Math.random() * (numSides || 6)))
+    for(var i = 0; i < numRolls; i++) {
+      output.push(this.rollOnce())
     }
     return output
+  }
+}
+
+const root = {
+  getDie: ({numSides}) => {
+    return new RandomDie(numSides || 6)
   }
 }
 
@@ -26,4 +46,4 @@ app.use('/graphql', graphqlHTTP({
 }))
 
 app.listen(4000)
-console.log('Running a GraphQL API sever')
+console.log('Running a GraphQL API sever...')
