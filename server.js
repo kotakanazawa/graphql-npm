c1b6acbdfa2ffc777d708ba76979ffb3ceb1c4e2
Kota Kanazawa <kota0818@gmail.com>
2021-06-19 14:59:27 +0900

    引数の受け渡し

--- server.js
+++ server.js
@@ -4,22 +4,18 @@ const { buildSchema } = require('graphql')
 
 const schema = buildSchema(`
   type Query {
-    quoteOfTheDay: String
-    random: Float!
-    rollThreeDice: [Int]
+    rollDice(numDice: Int!, numSides: Int): [Int]
   }
 `)
 
 const root = {
-  quoteOfTheDay: () => {
-    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within'
-  },
-  random: () => {
-    return Math.random()
-  },
-  rollThreeDice: () => {
-    return [2,4,6].map(() => 1 + Math.floor(Math.random() * 6))
-  },
+  rollDice: ({numDice, numSides}) => {
+    let output = []
+    for(var i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)))
+    }
+    return output
+  }
 }
 
 const app = express()
